b0038e02129043983bd383d8b5fc321387ed9ecb
Juan Roa <jdroa92@gmail.com>
2023-05-01 20:07:45 -0500

    finished todo mocks structure, card and autocomplete

--- src/components/ToDo/index.js
+++ src/components/ToDo/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import { isEmpty, isNil } from 'ramda';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import { Box, Container, Grid, MenuItem } from '@mui/material';
+import { Box, Button, Card, CardContent, CardActions, Container, Grid, MenuItem, Typography } from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { toDos as filteredOptions, sortByOptions } from '../../constants/data/toDoMocks';
@@ -11,13 +12,20 @@ const ToDo = () => {
 
 	// const filteredOptions = [];
 	const [inputValue, setInputValue] = useState('');
+	const [selectedOption, setSelectedOption] = useState(null);
 	const [sortBy, setSortBy] = useState('');
 
 	const handleChangeSortBy = (event) => {
 		setSortBy(event.target.value);
 	};
 
+	const handleOptionChange = (event, newOption) => {
+		setSelectedOption(newOption);
+	};
 
+	const getTaskTitle = (tasks) => `${tasks[0]} ${tasks[1] || ''} ${tasks[2] || ''}`
+
+	console.log(selectedOption)
 
 	return (
 		<Container
@@ -27,7 +35,7 @@ const ToDo = () => {
 				display: 'flex',
 				justifyContent: 'center',
 				alignItems: 'center',
-				minHeight: 'calc(50vh - 64px)',
+				minHeight: 'calc(80vh - 64px)',
 
 			}}
 		>
@@ -35,10 +43,14 @@ const ToDo = () => {
 				<Grid item xs={12} sm={12} md={9}>
 
 					<Autocomplete
+						style={{ marginBottom: '5rem' }}
 						fullWidth
+						onChange={handleOptionChange}
 						options={filteredOptions}
+						getOptionLabel={(option) => getTaskTitle(option.tasks)}
 						inputValue={inputValue}
 						onInputChange={(event, newInputValue) => {
+							console.log('input value', newInputValue)
 							setInputValue(newInputValue);
 						}}
 						renderOption={(props, option, { selected }) => (
@@ -47,7 +59,7 @@ const ToDo = () => {
 								style={styles.searchList(option.value)}
 							>
 								<div>
-									<div style={styles.strong}>{option.task}</div>
+									<div style={styles.strong}>{getTaskTitle(option.tasks)}</div>
 									<div>{option.uhid}</div>
 									<div>{option.dateofbirth}</div>
 									<div>Weight {option.birth_weight} g</div>
@@ -58,10 +70,40 @@ const ToDo = () => {
 							<TextField {...params} label="To Do List" variant="outlined" />
 						)}
 					/>
+
+					{!isEmpty(inputValue) && !isNil(selectedOption) ? (
+						<Card sx={{ minWidth: 275 }}>
+							<CardContent>
+								{/* <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+									Word of the Day
+								</Typography> */}
+
+								<>
+									<Typography variant="body1" component="header" style={styles.strong}>
+										{inputValue}
+									</Typography>
+									<Typography color="text.secondary">
+										{selectedOption.uhid}
+									</Typography>
+									<Typography variant="body2">
+										{selectedOption.dateofbirth}
+									</Typography>
+									<Typography variant="body3">
+										{selectedOption.birth_weight}
+									</Typography>
+								</>
+
+
+							</CardContent>
+							{/* <CardActions>
+								<Button size="small">Learn More</Button>
+							</CardActions> */}
+						</Card>
+					) : null}
 				</Grid>
 			</Grid>
 
-		</Container>
+		</Container >
 
 
 	);
--- src/constants/data/toDoMocks.js
+++ src/constants/data/toDoMocks.js
@@ -1,32 +1,64 @@
 export const toDos = [
      {
         value: 1,
-        task: 'S2S care',
+        tasks: ["Lactation consultation <=72 hours",
+        "Skin to skin care < 14 days"],
         uhid: 'P19997645',
         dateofbirth: 'DoB 2.3.23',
         birth_weight: 2689,
      },
      {
         value: 2,
-        task: 'S2S care',
+        tasks: ["Skin to skin care < 21 days"],
         uhid: 'P19997642',
         dateofbirth: 'DoB 28.2.23',
         birth_weight: 2675,
      },
      {
         value: 3,
-        task: 'Set up Lactation Appt',
+        tasks: ["Lactation consultation <=48 hours",
+        "Skin to skin care < 7 days", "Third task element"],
         uhid: 'P19997645',
         dateofbirth: 'DoB 2.3.23',
         birth_weight: 2675,
      },
      {
         value: 4,
-        task: 'Set up Lactation Appt',
+        tasks: ['Set up Lactation Appt'],
         uhid: 'P19997645',
         dateofbirth: 'DoB 2.3.23',
         birth_weight: 2689,
      },
+     {
+      value: 5,
+      tasks: ["Lactation consultation <=12 hours",
+      "Skin to skin care < 7 days"],
+      uhid: 'P19997645',
+      dateofbirth: 'DoB 2.3.23',
+      birth_weight: 2689,
+   },
+   {
+      value: 6,
+      tasks: ["Skin to skin care < 3 days"],
+      uhid: 'P19997642',
+      dateofbirth: 'DoB 28.2.23',
+      birth_weight: 2675,
+   },
+   {
+      value: 7,
+      tasks: ["Lactation consultation <=24 hours",
+      "Skin to skin care < 5 days", "Third task element"],
+      uhid: 'P19997645',
+      dateofbirth: 'DoB 2.3.23',
+      birth_weight: 2675,
+   },
+   {
+      value: 8,
+      tasks: ['Set up Lactation Appt 2'],
+      uhid: 'P19997645',
+      dateofbirth: 'DoB 2.3.23',
+      birth_weight: 2689,
+   },
 ];
 
 export const sortByOptions = [
