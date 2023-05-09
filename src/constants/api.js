ea4ddb519a22ca338c1757bc9091dbd95ed12bda
Juan Roa <jdroa92@gmail.com>
2023-05-09 14:15:11 -0500

    change api url

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const apiUrl = 'http://api.neonatologyoutcomes.com';
+export const apiUrl = 'https://api.neonatologyoutcomes.com';
 // export const apiUrl = 'http://localhost:8080';
 
 export const endpoints = {
