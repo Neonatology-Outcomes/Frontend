5e64c7b1e0e2a6b4b0735672afba50d3cdb5310a
Juan Roa <jdroa92@gmail.com>
2023-05-09 03:15:58 -0500

    date-fns added, fixed more UI and logic for assessment

--- src/pages/Assessment/index.js
+++ src/pages/Assessment/index.js
@@ -20,21 +20,20 @@ import { styles } from './styles';
 import { getVariant, getVariantStyle } from '../../utils';
 
 function Assessment() {
-  // Add your state and other logic here
-  // const [activeState, setActiveState] = useState([false, false, false]);
   // const [dateTime, setDateTime] = useState(new Date().toLocaleString());
   const [assessmentDate] = useState(new Date());
   // const [assessmentDateTime, setAssessmentDateTime] = useState(new Date());
   const [dayOfLife, setDayOfLife] = useState(1);
-  const [actionDate, setActionDate] = useState(new Date().toISOString().slice(0, -8));
   const [actionDateTime, setActionDateTime] = useState(new Date().toISOString().slice(0, -8));
+  // const [actionDateTime, setActionDateTime] = useState(new Date().toISOString().slice(0, -8));
   const [selectedBundle, setSelectedBundle] = useState('At Admission');
-  // const [actionRadio, setActionRadio] = useState('');
   const [checkboxes, setCheckboxes] = useState({
     firstSkinToSkin: false,
     scheduleLactationConsultant: false,
     firstOralFeeding: false,
   });
+
+  console.log(checkboxes);
 
   const handleCheckboxChange = (event) => {
     setCheckboxes({
@@ -47,9 +46,15 @@ function Assessment() {
     setSelectedBundle(bundle);
   };
 
-  // const handleSetAssessmentDate = (event) => {
-  //   setAssessmentDate(event.target.value);
-  // };
+  const handleSetAssessmentDateTime = (event) => {
+    console.log(event.target.value);
+    setActionDateTime(event.target.value);
+  };
+
+  const handleSetActionDateTime = (event) => {
+    console.log(event.target.value);
+    setActionDateTime(event.target.value);
+  };
 
   const updateDayOfLife = () => {
     const currentDate = new Date();
@@ -58,24 +63,12 @@ function Assessment() {
     setDayOfLife(dayDifference); // Add 1 to the difference because the day of life starts from 1
   };
 
-  // const handleChangeRadio = (event) => {
-  //   setActionRadio(event.target.value);
-  // };
-
   useEffect(() => {
     updateDayOfLife();
   }, [assessmentDate]);
 
-  // useEffect(() => {
-  //   updateDayOfLife();
-  // }, [assessmentDateTime]);
-
   return (
     <Container style={styles.container}>
-      {/* <Typography variant="h4" component="h1">
-        Assessment
-      </Typography> */}
-
       <Box
         component="div"
         display="flex"
@@ -112,26 +105,6 @@ function Assessment() {
           </Button>
         </Box>
       </Box>
-      {/* 
-      <Box component="div" display="flex" justifyContent="center" style={styles.midSection}>
-        <Typography variant="h5" component="h2">
-          Assessment
-        </Typography>
-        <TextField
-          style={styles.textField}
-          label="Date and Time of Assessment"
-          type="text"
-          value={dateTime}
-          readOnly
-        />
-        <TextField
-          style={styles.textField}
-          label="Day of Life"
-          type="number"
-          value={dayOfLife}
-          onChange={(e) => setDayOfLife(e.target.value)}
-        />
-      </Box> */}
       <Divider style={styles.divider} />
       <Box
         component="div"
@@ -149,8 +122,6 @@ function Assessment() {
           justifyContent="flex-start"
           style={{ marginBottom: '30px', width: '100%' }}
         >
-          {' '}
-          {/* Add this line for spacing */}
           <TextField
             style={styles.textField}
             label="Date and Time of Assessment"
@@ -158,8 +129,8 @@ function Assessment() {
             InputLabelProps={{
               shrink: true,
             }}
-            value={actionDate}
-            onChange={(e) => setActionDate(new Date(e.target.value))}
+            value={actionDateTime}
+            onChange={handleSetAssessmentDateTime}
           />
           <TextField
             style={styles.textField}
@@ -190,10 +161,9 @@ function Assessment() {
             shrink: true,
           }}
           value={actionDateTime}
-          onChange={(e) => setActionDateTime(e.target.value)}
+          onChange={handleSetActionDateTime}
         />
         <FormControl component="fieldset">
-          {/* <FormLabel component="legend">Actions</FormLabel> */}
           <FormGroup>
             <FormControlLabel
               control={
