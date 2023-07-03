6c1e5dad2b716eb1465325c93d23196b7b412028
Juan Roa <jdroa92@gmail.com>
2023-07-03 10:57:28 -0500

    using google cloud container neonatology outcomes API

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,6 @@
 // export const apiUrl = 'https://api.neonatologyoutcomes.com';
-export const apiUrl = 'http://localhost:8080';
+// export const apiUrl = 'http://localhost:8080';
+export const apiUrl = 'https://neonaologybackend-ec7oexxkma-uc.a.run.app';
 
 export const endpoints = {
   login: '/login',
